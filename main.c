dbca96f867eb2ed7230d46fa23097d6ebe6907c4
Lobhas Tidke <lobhastidke9a64@gmail.com>
2026-07-06 15:42:22 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int num;
+printf("====== Password Generator ======");
+  printf("Enter password length:\n");
+  scanf("%d",&num);
+//logic behind this ->>
+void generatePassword();
+printf("Generated Password: ");
+printf("%f",genp);
