b8c6e5ca7e0b50fc2250235b32da62365d90bd87
Lobhas Tidke <lobhastidke9a64@gmail.com>
2026-07-07 15:17:04 +0530

    Implement password generator functionality

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 #include<conio.h>
-int num;
-printf("====== Password Generator ======");
-  printf("Enter password length:\n");
+int main()
+{
+	// give num and i == 0 , so they cant get garbage value assigned
+int num=0;
+int i=0;
+char genp[500];
+srand(time(NULL));
+printf("====== Password Generator ======\n");
+  printf("Enter password length:");
   scanf("%d",&num);
 //logic behind this ->>
-void generatePassword();
-printf("Generated Password: ");
-printf("%f",genp);
+char pool[500] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}<>?/|";
+int poolsize = 89;
+for(i=0;i<num;i++)
+{
+	int index= rand()% poolsize;
+	genp[i]=pool[index];
+}
+genp[num] = '\0';
+printf("Generated Password: %s\n",genp);
+
+}
